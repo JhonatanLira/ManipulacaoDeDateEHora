fb58d0c74fa9751f5aa1833abc6366dcc023ee64
Jhonatan <jhonatanlira@gmail.com>
2024-09-04 12:40:22 -0300

    Como usar a classe Date

---
+++ src/date/ManipulaDate.java
@@ -0,0 +1,11 @@
+package date;
+
+import java.util.Date;
+
+public class ManipulaDate {
+    public static void main(String[] args) {
+        Date date = new Date(0);
+        System.out.println(date.getTime());//apresenta a data em milisegundos
+
+    }
+}
