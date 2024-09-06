933f1ec4e553a98e38de03827de5ef6a4674a887
Jhonatan <jhonatanlira@gmail.com>
2024-09-06 16:28:58 -0300

    Como usar o Before e After do Date

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ManipulacaoDeDateEHora
+# Manipulacao De Date & Hora em Java
+
+<table>
+<thead>
+<tr>
+<th>Aulas</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Como usar a classe Date</td>
+</tr>
+<tr>
+<td>Como usar o Before e After do Date</td>
+</tr>
+<tr>
+<td>Como usar Calendar</td>
+</tr>
+
+</tbody>
+</table>
--- src/date/ManipulaDate.java
+++ src/date/ManipulaDate.java
@@ -4,8 +4,19 @@ import java.util.Date;
 
 public class ManipulaDate {
     public static void main(String[] args) {
-        Date date = new Date(0);
-        System.out.println(date.getTime());//apresenta a data em milisegundos
+
+        Date date1 = new Date();
+        System.out.println("Date1" + date1.getTime());//apresenta a data em milisegundos
+
+        Date date2 = new Date(System.currentTimeMillis() + 1000);
+        System.out.println("Date2" + date2.getTime());
+
+        if (date1.before(date2)) {
+            System.out.println("Data1 antes de data2");
+        }
+        if (date2.after(date1)) {
+            System.out.println("Data2 depois de data1");
+        }
 
     }
 }
